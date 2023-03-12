048a0db5ff8d7fd5f7fe6e8588e5bf97c463a1a6
KhayKhun <99352078+KhayKhun@users.noreply.github.com>
2023-03-12 20:56:09 +0630

    mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-
+import favicon from './public/favicon.ico';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  head: {
+    links: [
+      { rel: 'icon', type: 'image/x-icon', href: favicon },
+    ],
+  },
 });
