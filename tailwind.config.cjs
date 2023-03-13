8fa930e847e1f701427deb0f961a8c16c8ff61cd
KhayKhun <99352078+KhayKhun@users.noreply.github.com>
2023-03-13 22:33:24 +0630

    ag

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	publicPath: './',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	mode: 'jit',
 	purge: ['./src/**/*.astro'],
 	theme: {
 		extend: {},
