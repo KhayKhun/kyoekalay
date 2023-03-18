7aca8199d0528e035dc53418febb83b4aedeb497
KhayKhun <99352078+KhayKhun@users.noreply.github.com>
2023-03-18 19:07:23 +0630

    fddos

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://khaykhun.github.io/',
-  base: '/kyoekalay',
+  base: '/kyoekalay/dist',
 });
