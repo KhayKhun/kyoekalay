6ea9776dbe9bc3f02b1fc32f98a63aec48a788dd
KhayKhun <99352078+KhayKhun@users.noreply.github.com>
2023-03-18 19:29:07 +0630

    oashviursel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://KhayKhun.github.io/',
-  base: '/kyoekalay',
+  site: 'https://KhayKhun.github.io/kyoekalay',
+  base: '/dist/',
 });
