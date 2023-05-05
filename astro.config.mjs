54385b0f995f2a90e4e2f218b99ee8eb92f27ad1
KhayKhun <99352078+KhayKhun@users.noreply.github.com>
2023-05-05 23:56:30 +0630

    fix .gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-// import favicon from './public/favicon.ico';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://KhayKhun.github.io'
+  site: 'https://kyoekalay.onrender.com'
 });
